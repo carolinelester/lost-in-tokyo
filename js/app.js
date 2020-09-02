8e96ab6e5f1c06809b491252f2cd0fcc3e660cad
Caroline Lester <carolineclester@gmail.com>
2020-09-01 23:21:02 -0400

    beginnings of react app

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
-const Hello = props => <h1>Hello {props.name}</h1>;
+const Intro = () => <div>
+    <div>
+    Lost in Tokyo is a directory of fun places to see, play in and explore, in Tokyo, Japan.
+    </div>
+    <div>
+    From museums and galleries, to Robot Restaurants and kitten cafes, Tokyo is the gift that keeps on giving. Dattebayo!
+    </div>
+    </div>;
 
+const App = () => (
+    <div>
+        <div className="min-vh-100 ph4 flex flex-column">
+            {}
+            <Intro />
+        </div>
+        <div className="flex flex-wrap container">
+            {}
+        </div>
+    </div>
+)
 ReactDOM.render(<Hello name="Lawrence" />, document.getElementById('root'));
